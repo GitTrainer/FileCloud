e38881238329b96b58f8949b751e0e695e5d218e
ufoit21 <dangkhanhit@gmail.com>
2013-01-10 16:54:53 +0700

    sign-in-out commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,12 @@ class User < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   	has_secure_password
+
+  	before_save { |user| user.email = email.downcase }
+  	before_save :create_remember_token
+
+  	private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Filecloud::Application.routes.draw do
   resources :users
   root to: 'users#index'
+  
   match '/activate',to: 'users#activate'
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+  
+
+
+
+
+
+
+
+
+
+
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
