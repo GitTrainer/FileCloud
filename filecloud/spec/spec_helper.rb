1de1de041470cffe47b10aed86ed9fd892a9cfe1
kien <kien@buikien.(none)>
2013-01-21 15:22:54 +0700

    Push phase 1 to prepare merge

--- filecloud/spec/spec_helper.rb
+++ filecloud/spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'rspec/autorun'
 require 'capybara/rails'
 require 'factory_girl'
 require 'factory_girl_rails'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
