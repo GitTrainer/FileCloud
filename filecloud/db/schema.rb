f523b28bf08d322e0c44d431991864592a804ae1
Bui Trung Kien <bui.trung.kien@framgia.com>
2013-01-22 10:17:03 +0700

    Fix GUI and create user_id in folder table

--- filecloud/db/schema.rb
+++ filecloud/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130118031512) do
+ActiveRecord::Schema.define(:version => 20130122030332) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20130118031512) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "file"
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
