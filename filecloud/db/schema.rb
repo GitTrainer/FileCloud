e747ac1331e8a9e8f9e3801b126d61df54efa728
DavidTin57 <tuanthuasacit@gmail.com>
2013-01-25 15:55:02 +0700

    fix

--- filecloud/db/schema.rb
+++ filecloud/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124062718) do
+ActiveRecord::Schema.define(:version => 20130121045929) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -46,8 +46,6 @@ ActiveRecord::Schema.define(:version => 20130124062718) do
     t.datetime "upload_updated_at"
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
-    t.integer  "folder_id"
-    t.string   "image"
   end
 
   create_table "users", :force => true do |t|
