a05aabce37820ffb61f7c5e3023a7b0ee9b6dd27
Bui Trung Kien <bui.trung.kien@framgia.com>
2013-01-25 09:38:34 +0700

    delete AddColumnToFoldersharing

--- filecloud/db/migrate/20130122085813_add_column_to_folder_sharing.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToFolderSharing < ActiveRecord::Migration
-  def change
-  	add_column :foldersharings, :folder_id, :integer
-  	add_column :foldersharings, :shared_user_id, :integer
-  end
-end
