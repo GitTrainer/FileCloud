bcb101b07c35ffe085a8068f31b9eceda3d6d8f5
Bui Trung Kien <bui.trung.kien@framgia.com>
2013-01-25 13:33:14 +0700

    fix bug when create category and fix authentication bug

--- filecloud/app/controllers/categories_controller.rb
+++ filecloud/app/controllers/categories_controller.rb
@@ -1,104 +1,88 @@
 class CategoriesController < ApplicationController
   before_filter :signed_in_user
-  before_filter :correct_user, only:[:edit,:update, :create]
-  before_filter :admin_user, only: :destroy
 
-def index
-	if ( @new_category.nil?)
-		@new_category = Category.new
+	def index
+		if (@new_category.nil?)
+			@new_category = Category.new
+		end
+		  @categories = Category.all
+		  respond_to do |format|
+		    format.html { render action: "index"}
+		    format.js {render js: @new_category }
+		    format.js {render js: @categories }
+		  end
 	end
-	@categories = Category.all
-    respond_to do |format|
-      format.html { render action: "index"}
-      format.js {render js: @new_category }
-      format.js {render js: @categories }
+
+	def new
+	  @new_category = Category.new
+	end
+
+	def create
+	  @new_category = Category.new(params[:category])
+		@categories = Category.all
+		respond_to do |format|
+		  if @new_category.save
+     	  @new_category = nil
+     	  format.html { redirect_to "/categories"}
+          format.js {render js: @new_category }
+          format.js {render js: @categories }
+      else
+        format.html { render action: "index"}
+        format.js {render js: @new_category.errors, status: :unprocessable_entity}
+        format.js {render js: @categories }
+	    end
     end
-end
+	end
 
-def new
- @new_category = Category.new
-end
-
-def create
-	 @new_category = Category.new(params[:category])
-	  @categories = Category.all
-	  respond_to do |format|
-       if @new_category.save
-       	 @new_category = nil
-       	 format.html { redirect_to "/categories"}
-              format.js {render js: @new_category }
-              format.js {render js: @categories }
-       else
-
-         format.html { render action: "index"}
-         format.js {render js: @new_category.errors, status: :unprocessable_entity}
-         format.js {render js: @categories }
-		 end
-    end
-end
-
-def show
-	@category = Category.find(params[:id])
-	@folders = Folder.where(:category_id => params[:id])
-          respond_to do |format|
-             format.html { render action: "show"}
-              format.js {render js: @category }
-             end
-
-end
-
-def edit
-	@categories = Category.all
-    @new_category = Category.find(params[:id])
+	def show
+		@category = Category.find(params[:id])
+		@folders = Folder.where(:category_id => params[:id])
     respond_to do |format|
-             format.html { render action: "index"}
-             format.js {render js: @new_category}
-             format.js {render js: @categories }
-             end
-end
+      format.html { render action: "show"}
+      format.js {render js: @category }
+    end
+	end
 
-def update
-	@new_category = Category.find(params[:id])
-	@categories = Category.all
-       respond_to do |format|
-          if @new_category.update_attributes(params[:category])
-			 @new_category = nil
-              format.html { redirect_to "/categories"}
-             format.js { render js: @new_category }
-          else
-             format.html {  render action: "index"}
-             format.js {render js: @new_category.errors, status: :unprocessable_entity}
-             format.js {render js: @categories }
-          end
-       end
-end
+	def edit
+		@categories = Category.all
+		@new_category = Category.find(params[:id])
+		respond_to do |format|
+		  format.html { render action: "index"}
+		  format.js {render js: @new_category}
+		  format.js {render js: @categories }
+		end
+	end
 
-def destroy
-	@category = Category.find(params[:id])
-	@category.destroy
-	respond_to do |format|
-             format.html { redirect_to "/categories"}
-         end
-end
+	def update
+		@new_category = Category.find(params[:id])
+		@categories = Category.all
+		  respond_to do |format|
+		    if @new_category.update_attributes(params[:category])
+				  @new_category = nil
+          format.html { redirect_to "/categories"}
+          format.js { render js: @new_category }
+		    else
+          format.html {  render action: "index"}
+          format.js {render js: @new_category.errors, status: :unprocessable_entity}
+          format.js {render js: @categories }
+        end
+      end
+	end
 
+	def destroy
+		@category = Category.find(params[:id])
+		@category.destroy
+		respond_to do |format|
+		  format.html { redirect_to "/categories"}
+		end
+	end
 
   private
-
     def signed_in_user
       unless signed_in?
         store_location
         redirect_to signin_url, notice: "Please sign in."
       end
     end
-
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
-    end
-    def admin_user
-      @user=User.find(params[:id])
-      redirect_to (root_path) unless current_user.admin?
-        
-      end
 
 end
--- filecloud/app/controllers/folders_controller.rb
+++ filecloud/app/controllers/folders_controller.rb
@@ -1,129 +1,118 @@
 class FoldersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :correct_user_create, only: [:index, :create]
 
-   def correct_user_create
-      if params[:user_id].to_s != current_user.id.to_s
-         redirect_to root_path
-      end
-   end
+  def correct_user_create
+    if params[:user_id].to_s != current_user.id.to_s
+      redirect_to root_path
+    end
+  end
+
   def correct_user
       if Folder.where(:user_id=> current_user.id , :id => params[:id]).blank?
          redirect_to root_path
       end
    end
-def index
-	@foldersharings = Foldersharing.all
-	@folders = Folder.where(:user_id => current_user)
-	if ( @new_folder.nil?)
+
+	def index
+		@foldersharings = Foldersharing.all
+		@folders = Folder.where(:user_id => current_user)
+		if ( @new_folder.nil?)
+			@new_folder = Folder.new
+		end
+      respond_to do |format|
+        format.html { render action: "index"}
+        format.js {render js: @new_folder }
+        format.js {render js:  @folders }
+      end
+	  end
+
+	def new
+  	@foldersharings = Foldersharing.all
 	  @new_folder = Folder.new
 	end
-          respond_to do |format|
-             format.html { render action: "index"}
-              format.js {render js: @new_folder }
-               format.js {render js:  @folders }
-             end
-end
 
+	def create
+		@foldersharings = Foldersharing.all
+		@new_folder = Folder.new(params[:folder])
+		@folders = Folder.where(:user_id => current_user)
+		respond_to do |format|
+		  if @new_folder.save
+			  @new_folder = nil
+				format.html { redirect_to "/folders/?user_id=" + params[:folder][:user_id]}
+		      format.js {render js: @new_folder }
+		      format.js {render js: @folders }
+		  else
+		    format.html { render action: "index"}
+		    format.js {render js: @new_folder.errors, status: :unprocessable_entity}
+		    format.js {render js: @folders }
+		  end
+		end
+	end
 
+	def edit
+		@foldersharings = Foldersharing.all
+		@folders = Folder.where(:user_id => current_user)
+		@new_folder = Folder.find(params[:id])
+		respond_to do |format|
+			format.html { render action: "index"}
+			format.js {render js: @new_folder}
+			format.js {render js: @folders }
+		end
+	end
 
-def new
-@foldersharings = Foldersharing.all
- @new_folder = Folder.new
-end
+	def show
+		@foldersharings = Foldersharing.all
+		respond_to do |format|
+			if current_user.id.to_s == params[:user_id].to_s
+				@folder = Folder.find(params[:id])
+				format.html { render action: "show"}
+				format.js {render js: @folder }
+			else
+				if Foldersharing.where(:shared_user_id => current_user.id, :folder_id => params[:id]).exists?
+					@folder = Folder.find(params[:id])
+					format.html { render action: "show"}
+				  format.js {render js: @folder }
+			 	else
+			 		format.html { redirect_to root_path }
+				end
+			end
+		end
+	end
 
-def create
-@foldersharings = Foldersharing.all
-	 @new_folder = Folder.new(params[:folder])
-	@folders = Folder.where(:user_id => current_user)
- respond_to do |format|
-       if @new_folder.save
+	def update
+		@foldersharings = Foldersharing.all
+		@new_folder = Folder.find(params[:id])
+		@folders = Folder.where(:user_id => current_user)
+		respond_to do |format|
+			if @new_folder.update_attributes(params[:folder])
+				@new_folder = nil
+		    format.html { redirect_to "/folders/?user_id=" + params[:folder][:user_id]}
+		    format.js { render js: @new_folder }
+		  else
+		    format.html {  render action: "index"}
+		    format.js {render js: @new_folder.errors, status: :unprocessable_entity}
+		    format.js {render js: @folders }
+		  end
+		end
+	end
 
-			 	 @new_folder = nil
-			 	format.html { redirect_to "/folders/?user_id=" + params[:folder][:user_id]}
-              format.js {render js: @new_folder }
-              format.js {render js: @folders }
-       else
+	def destroy
+		@foldersharings = Foldersharing.all
+		@user_id = Folder.find(params[:id]).user_id
+		@folder = Folder.find(params[:id])
+		@folder.destroy
+		respond_to do |format|
+		  format.html { redirect_to "/folders/?user_id=" + @user_id.to_s }
+    end
+	end
 
-         format.html { render action: "index"}
-         format.js {render js: @new_folder.errors, status: :unprocessable_entity}
-         format.js {render js: @folders }
-         end
-       end
-end
-
-def edit
-@foldersharings = Foldersharing.all
-	@folders = Folder.where(:user_id => current_user)
-    @new_folder = Folder.find(params[:id])
-    respond_to do |format|
-             format.html { render action: "index"}
-             format.js {render js: @new_folder}
-             format.js {render js: @folders }
-             end
-end
-
-def show
-@foldersharings = Foldersharing.all
-	@folder = Folder.find(params[:id])
-          respond_to do |format|
-             format.html { render action: "show"}
-              format.js {render js: @folder }
-             end
-end
-
-def update
-
-@foldersharings = Foldersharing.all
-
-	@new_folder = Folder.find(params[:id])
-	@folders = Folder.where(:user_id => current_user)
-       respond_to do |format|
-         if @new_folder.update_attributes(params[:folder])
-		  @new_folder = nil
-         format.html { redirect_to "/folders/?user_id=" + params[:folder][:user_id]}
-          format.js { render js: @new_folder }
-
-        else
-             format.html {  render action: "index"}
-             format.js {render js: @new_folder.errors, status: :unprocessable_entity}
-             format.js {render js: @folders }
-        end
-         end
-end
-
-def destroy
-
-@foldersharings = Foldersharing.all
-
-  @user_id = Folder.find(params[:id]).user_id
-	@folder = Folder.find(params[:id])
-	@folder.destroy
-	respond_to do |format|
-             format.html { redirect_to "/folders/?user_id=" + @user_id.to_s }
-         end
-end
-
-def share
-
-end
-	@users = User.all
-
-
-
-
-    private
-
+  private
     def signed_in_user
       unless signed_in?
         store_location
         redirect_to signin_url, notice: "Please sign in."
       end
     end
-
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
-    end
-
 end
