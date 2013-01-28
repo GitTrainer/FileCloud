24cc3efe5e85db4a82cb285b90ba9e29bf06f3f4
DavidTin57 <tuanthuasacit@gmail.com>
2013-01-28 14:43:50 +0700

    edit some function

--- filecloud/app/controllers/registrations_controller.rb
+++ filecloud/app/controllers/registrations_controller.rb
@@ -1,6 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
-
-  # ovverride #create to respond to AJAX with a partial
+  
   def create
     build_resource
     if resource.save
@@ -27,7 +26,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def after_sign_up_path_for(resource)
-    # the page new users will see after sign up (after launch, when no invitation is needed)
     redirect_to root_path
   end
 
