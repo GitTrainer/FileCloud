a26cebfc574ad0c98bd34c985c1ede34babc7c0d
vandung <vandung@vandung.(none)>
2013-02-18 15:59:19 +0700

    search done user for ajax

--- filecloud/app/controllers/users_controller.rb
+++ filecloud/app/controllers/users_controller.rb
@@ -8,10 +8,10 @@ class UsersController < ApplicationController
 
   def index
     @users = User.search(params[:search])
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
-    end
+    # respond_to do |format|
+    #   format.html # index.html.erb
+    #   format.json { render json: @users }
+    # end
   end
 
   # GET /users/1
