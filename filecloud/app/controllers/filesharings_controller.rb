c39c890eb79de991ec477e04ccdc76c54b7bfa6f
Bui Trung Kien <bui.trung.kien@framgia.com>
2013-03-01 18:25:05 +0700

    Done using colorbox to share file and using delayed_job when sending email.

--- filecloud/app/controllers/filesharings_controller.rb
+++ filecloud/app/controllers/filesharings_controller.rb
@@ -13,9 +13,9 @@ class FilesharingsController < ApplicationController
 
 	def create
 		seen_ids = params[:seen].collect {|id| id.to_i} if params[:seen]
+		@file_id = params[:file_id]
 		if !seen_ids.nil?
-			@folder_id = Filestream.find(params[:filesharing][:file_id]).folder_id
-			@file_id = params[:filesharing][:file_id]
+			@folder_id = Filestream.find(@file_id).folder_id
 			activated_ids = params[:activated].collect {|id| id.to_i} if params[:activated]
 		  if !activated_ids.nil?
 		 	  if Filesharing.where(:file_id => @file_id).exists?
@@ -24,7 +24,7 @@ class FilesharingsController < ApplicationController
 		    activated_ids.each do |activated_id|
       	  @file_share = Filesharing.new(:file_id => @file_id, :shared_user_id => activated_id)
 		      @file_share.save!
-		      UserMailer.delay.share_file(activated_id,@file_id)
+		      UserMailer.delay({:run_at => 10.seconds.from_now}).share_file(activated_id,@file_id)
 		 	  end
 		 	  flash[:success] = "Share file successfully"
 		 	  redirect_to ("/folders/"+ @folder_id.to_s)
@@ -34,9 +34,9 @@ class FilesharingsController < ApplicationController
 		    redirect_to ("/folders/"+ @folder_id.to_s)
 		  end
 		else
-			@id = params[:filesharing][:folder_id].to_i
+			@folder_id = Filestream.find(@file_id).folder_id
 			flash[:notice] = "No member to share"
-			redirect_to "/folders/#{@id}"
+			redirect_to "/folders/#{@folder_id}"
 	  end
 	end
 end
